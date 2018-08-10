c5cfb289a566231175fed4d626cee0ec4acd37fb
Jad Ghalayini <jad.ghalayini@hotmail.com>
2018-08-10 00:34:22 -0400

    Added memory and stack

--- src/datapath/memory.v
+++ src/datapath/memory.v
@@ -26,7 +26,23 @@ module memory(
 
   `else
 
-  
+  stack_ram stack(
+    .address(address),
+    .clock(clock),
+    .data(value),
+    .wren(stack_store_enable),
+    .q(at_stack));
+
+  memory_ram memory(
+    .address_a(program_counter),
+    .address_b(address),
+    .clock(clock),
+    .data_a(16'hxxxx),
+    .data_b(value),
+    .wren_a(1'b0),
+    .wren_b(memory_store_enable),
+    .q_a(current_instruction),
+    .q_b(at_memory));
 
   `endif
 
